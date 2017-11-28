72217bec7de0be1161ec96451fc45ff15679ddf7
Matt Conway <matt@conwaysplace.com>
2017-11-28 09:46:32 -0500

    botchain deploys dependent contracts for now

--- migrations/1511878270_deploy_developer_record.js
+++
@@ -1,5 +0,0 @@
-const DeveloperRecord = artifacts.require("./DeveloperRecord.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(DeveloperRecord);
-};
--- migrations/1511878274_deploy_bot.js
+++
@@ -1,5 +0,0 @@
-const Bot = artifacts.require("./Bot.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Bot);
-};
