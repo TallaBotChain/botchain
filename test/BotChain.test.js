de60afe119e5bc5f855151956a67a01895e21e87
Mike Calvanese <mfcalvanese@gmail.com>
2017-12-06 15:20:38 -0500

    add stub tests for developer functions

---
+++ test/BotChain.test.js
@@ -0,0 +1,104 @@
+/* global describe it beforeEach artifacts contract */
+
+import { expect } from 'chai'
+import { web3 } from './helpers/w3'
+import tryAsync from './helpers/tryAsync'
+
+// const BotChain = artifacts.require('./BotChain.sol')
+const { accounts } = web3.eth
+
+contract('BotChain', () => {
+  describe('when deployed', () => {
+    let bc
+    beforeEach(async () => {
+      // bc = await newBotChain()
+    })
+
+    it('should...', async () => {
+      //
+    })
+  })
+
+  describe('addDeveloper()', () => {
+    describe('when given a valid address and valid hash', () => {
+      it('should add developer to mapping', async () => {
+        //
+      })
+    })
+
+    describe('when given an invalid hash', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+
+    describe('when called by non-owner', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+
+    describe('when given a 0x0 address', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+  })
+
+  describe('updateDeveloper()', () => {
+    describe('when given a valid address and valid hash', () => {
+      it('should update hash value in mapping', async () => {
+        //
+      })
+    })
+
+    describe('when given an invalid hash', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+
+    describe('when called by non-owner', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+
+    describe('when given a 0x0 address', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+  })
+
+  describe('removeDeveloper()', () => {
+    describe('when given a valid address that exists in mapping', () => {
+      it('should set mapping value for address to 0x0', async () => {
+        //
+      })
+    })
+
+    describe('when called by non-owner', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+
+    describe('when given a 0x0 address', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+
+    describe('when address does not exist in developer mapping', () => {
+      it('should throw', async () => {
+        //
+      })
+    })
+  })
+})
+
+async function newBotChain () {
+  // const bc = await tryAsync(BotChain.new())
+  // return bc
+}
