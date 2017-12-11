b52e7c9c91521723c353f760adf2c84b7dbc74f6
Mike Calvanese <mfcalvanese@gmail.com>
2017-12-11 12:55:53 -0500

    implement developer add/update/revoke

--- contracts/BotChain.sol
+++ contracts/BotChain.sol
@@ -7,6 +7,10 @@ import './BotOwnershipManager.sol';
 /// @dev Contract for managing ownership of BotChain resources
 contract BotChain is Ownable {
 
+  event DeveloperAdded(address developer, bytes32 data);
+  event DeveloperUpdated(address developer, bytes32 data);
+  event DeveloperApprovalRevoked(address developer);
+
   /// @dev The address of the BotOwnershipManager contract (ERC-721 implementation)
   ///  that handles ownership and transfer of Bots.
   BotOwnershipManager public botOwnershipManager;
@@ -16,7 +20,7 @@ contract BotChain is Ownable {
   mapping(address => bool) public developerToApproved;
 
   /// @dev A mapping of developer addresses to a hash of related data.
-  mapping(address => bytes) public developerToData;
+  mapping(address => bytes32) public developerToData;
 
   /// @dev An array of developer addresses.
   address[] public developers;
@@ -25,33 +29,56 @@ contract BotChain is Ownable {
   ///  which it owns by default.
   function BotChain() public {
     botOwnershipManager = new BotOwnershipManager();
+    developers.push(0x0);
+  }
+
+  /// @dev Checks if a developer is approved
+  /// @param _developer Address of developer to check for approval
+  function isApprovedDeveloper(address _developer) public returns(bool) {
+    return developerToApproved[_developer] == true;
   }
 
   /// @dev Adds a new developer. Only callable by owner.
   /// @param _developer The address of the developer to add
   /// @param _data A hash of the data associated with this developer
-  function addDeveloper(address _developer, bytes _data) onlyOwner public {
-    //
+  function addDeveloper(address _developer, bytes32 _data) onlyOwner external {
+    require(_developer != 0x0);
+    require(_data != 0x0);
+
+    developerToApproved[_developer] = true;
+    developerToData[_developer] = _data;
+    developers.push(_developer);
+
+    DeveloperAdded(_developer, _data);
   }
 
   /// @dev Updates an existing developer. Only callable by owner.
   /// @param _developer The address of the developer to update
   /// @param _data A hash of the data associated with this developer
-  function updateDeveloper(address _developer, bytes _data) onlyOwner public {
-    //
+  function updateDeveloper(address _developer, bytes32 _data) onlyOwner external {
+    require(_developer != 0x0);
+    require(_data != 0x0);
+
+    developerToData[_developer] = _data;
+
+    DeveloperUpdated(_developer, _data);
   }
 
   /// @dev Revokes approval for an existing developer. Only callable by owner.
   /// @param _developer The address of the developer to revoke approval for.
-  function revokeDeveloperApproval(address _developer) onlyOwner public {
-    //
+  function revokeDeveloperApproval(address _developer) onlyOwner external {
+    require(developerToApproved[_developer]);
+
+    developerToApproved[_developer] = false;
+
+    DeveloperApprovalRevoked(_developer);
   }
   
   /// @dev Creates a new bot. The new bot is owned by msg.sender, which needs
   ///  to be an approved developer.
   /// @param _botPublicKey The public key of the new Bot
   /// @param _botData A hash of the data associated with this Bot
-  function createBot(address _botPublicKey, bytes _botData) public {
+  function createBot(address _botPublicKey, bytes _botData) external {
     require(isApprovedDeveloper(msg.sender));
     botOwnershipManager.createBot(msg.sender, _botPublicKey, _botData);
   }
@@ -60,14 +87,9 @@ contract BotChain is Ownable {
   ///  to be an approved developer.
   /// @param _botPublicKey The public key of the new Bot
   /// @param _botData A hash of the data associated with this Bot
-  function updateBot(address _botPublicKey, bytes _botData) public {
+  function updateBot(address _botPublicKey, bytes _botData) external {
     require(isApprovedDeveloper(msg.sender));
     botOwnershipManager.updateBot(_botPublicKey, _botData);
   }
 
-  /// @dev Checks if a developer is approved
-  /// @param _developer Address of developer to check for approval
-  function isApprovedDeveloper(address _developer) public returns(bool) {
-    return developerToApproved[msg.sender] == true;
-  }
 }
--- test/BotChain.test.js
+++ test/BotChain.test.js
@@ -3,98 +3,153 @@
 import { expect } from 'chai'
 import { web3 } from './helpers/w3'
 import tryAsync from './helpers/tryAsync'
+import expectRevert from './helpers/expectRevert'
+import { hasEvent } from './helpers/event'
 import isNonZeroAddress from './helpers/isNonZeroAddress'
 
-const BotChain = artifacts.require('./BotChain.sol')
 const { accounts } = web3.eth
+const zeroAddr = '0x0000000000000000000000000000000000000000'
+const zeroHash = '0x0000000000000000000000000000000000000000000000000000000000000000'
+const devAddr = '0x72c2ba659460151cdfbb3cd8005ae7fbe68191b1'
+const devAddr2 = '0x85626d4d9a5603a049f600d9cfef23d28ecb7b8b'
+const nonOwnerAddr = accounts[1]
+const dataHash = web3.sha3('some data to hash')
+const updatedDataHash = web3.sha3('some modified data to hash')
+
+const BotChain = artifacts.require('./BotChain.sol')
 
 contract('BotChain', () => {
-  describe('when deployed', () => {
-    let bc
-    beforeEach(async () => {
-      bc = await newBotChain()
-    })
+  let bc
 
+  beforeEach(async () => {
+    bc = await newBotChain()
+  })
+
+  describe('when deployed', () => {
     it('should create a new BotManagerOwernship contract', async () => {
       const addr = await bc.botOwnershipManager.call()
       expect(isNonZeroAddress(addr)).to.equal(true)
+    })
+
+    it('should add 0x0 address as first developer in array', async () => {
+      const addr = await bc.developers.call(0)
+      expect(addr).to.equal(zeroAddr)
     })
   })
 
   describe('addDeveloper()', () => {
     describe('when given a valid address and valid hash', () => {
-      it('should add developer to mapping', async () => {
-        //
+      let txResult
+      beforeEach(async () => {
+        txResult = await bc.addDeveloper(devAddr, dataHash)
+      })
+
+      it('should add developer to data mapping', async () => {
+        const data = await bc.developerToData.call(devAddr)
+        expect(data).to.equal(dataHash)
+      })
+
+      it('should add developer to approved mapping', async () => {
+        const approved = await bc.developerToApproved.call(devAddr)
+        expect(approved).to.equal(true)
+      })
+
+      it('should add developer to array', async () => {
+        expect(await bc.developers.call(1)).to.equal(devAddr)
+      })
+
+      it('should log DeveloperAdded event', () => {
+        expect(hasEvent(txResult, 'DeveloperAdded')).to.equal(true)
       })
     })
 
-    describe('when given an invalid hash', () => {
+    describe('when given a 0x0 hash', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.addDeveloper(devAddr, zeroHash))
       })
     })
 
     describe('when called by non-owner', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.addDeveloper(devAddr, dataHash, { from: nonOwnerAddr }))
       })
     })
 
     describe('when given a 0x0 address', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.addDeveloper(zeroAddr, dataHash))
       })
     })
   })
 
   describe('updateDeveloper()', () => {
+    beforeEach(async () => {
+      await bc.addDeveloper(devAddr, dataHash)
+    })
+
     describe('when given a valid address and valid hash', () => {
+      let txResult
+      beforeEach(async () => {
+        txResult = await bc.updateDeveloper(devAddr, updatedDataHash)
+      })
+
       it('should update hash value in mapping', async () => {
-        //
+        expect(await bc.developerToData.call(devAddr)).to.equal(updatedDataHash)
+      })
+
+      it('should log DeveloperUpdated event', async () => {
+        expect(hasEvent(txResult, 'DeveloperUpdated')).to.equal(true)
       })
     })
 
-    describe('when given an invalid hash', () => {
+    describe('when given a 0x0 hash', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.updateDeveloper(devAddr, zeroHash))
       })
     })
 
     describe('when called by non-owner', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.updateDeveloper(devAddr, updatedDataHash, { from: nonOwnerAddr }))
       })
     })
 
-    describe('when given a 0x0 address', () => {
+    describe('when given a 0x0 developer address', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.updateDeveloper(zeroAddr, updatedDataHash))
       })
     })
   })
 
-  describe('removeDeveloper()', () => {
-    describe('when given a valid address that exists in mapping', () => {
-      it('should set mapping value for address to 0x0', async () => {
-        //
+  describe('revokeDeveloperApproval()', () => {
+    beforeEach(async () => {
+      await bc.addDeveloper(devAddr, dataHash)
+    })
+
+    describe('when given a valid developer address that is approved', () => {
+      let txResult
+      beforeEach(async () => {
+        txResult = await bc.revokeDeveloperApproval(devAddr)
+      })
+
+      it('should set approved to false', async () => {
+        expect(await bc.isApprovedDeveloper.call(devAddr)).to.equal(false)
+      })
+
+      it('should log DeveloperApprovalRevoked event', () => {
+        expect(hasEvent(txResult, 'DeveloperApprovalRevoked')).to.equal(true)
       })
     })
 
     describe('when called by non-owner', () => {
       it('should throw', async () => {
-        //
+        await expectRevert(bc.revokeDeveloperApproval(devAddr, { from: nonOwnerAddr }))
       })
     })
 
-    describe('when given a 0x0 address', () => {
+    describe('when given an address that is not an approved developer', () => {
       it('should throw', async () => {
-        //
-      })
-    })
-
-    describe('when address does not exist in developer mapping', () => {
-      it('should throw', async () => {
-        //
+        await expectRevert(bc.revokeDeveloperApproval(devAddr2))
       })
     })
   })
