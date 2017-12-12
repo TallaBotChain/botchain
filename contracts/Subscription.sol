284a275e814feb0cb366caa9eb741ff0bc1431c9
John Kelleher <johnpkelleher@gmail.com>
2017-12-12 08:50:55 -0500

    refactor subscription framework

--- contracts/Subscription.sol
+++ contracts/Subscription.sol
@@ -5,10 +5,7 @@ pragma solidity ^ 0.4.18
  * @dev Interface for Subscription contracts
  */
 contract Subscription {
-  event SubscriptionExtended(address subscriber, uint256 payment, uint256 timeExtended);
-
-  function setSubscriptionParameters(uint256 cost, uint256 duration, uint256 maxSubscription) public returns(bool);
-  function extendSubscription(address subscriber, uint256 payment) payable public returns (bool);
-  function checkSubscriptionStatus(address subscriber) public returns (bool);
-  function checkSubscriptionExpiration(address subscriber) public returns (uint256);
+  function setParameters(uint256 cost, uint256 duration, uint256 maxSubscription) external returns(bool);
+  function checkStatus(address subscriber) external returns (bool);
+  function checkExpiration(address subscriber) external returns (uint256);
 }
