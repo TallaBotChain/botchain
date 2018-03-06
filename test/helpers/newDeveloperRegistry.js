200b2093ef63d9cc32a7a30bfc58bda8475fbb92
alexfigtree <alexandra.higuera@gmail.com>
2018-03-06 14:03:57 -0500

    Developer registry additions

--- test/helpers/newDeveloperRegistry.js
+++ test/helpers/newDeveloperRegistry.js
@@ -5,13 +5,17 @@ import _ from 'lodash'
 const PublicStorage = artifacts.require('./PublicStorage.sol')
 const DeveloperRegistry = artifacts.require('./DeveloperRegistry.sol')
 const DeveloperRegistryDelegate = artifacts.require('./DeveloperRegistryDelegate.sol')
+const BotCoin = artifacts.require('./BotCoin.sol');
 
 export default async function newDeveloperRegistry () {
   const publicStorage = await PublicStorage.new()
   const developerRegistryDelegate = await DeveloperRegistryDelegate.new()
+  const botCoinAddress = await BotCoin.new()
+
   let developerRegistry = await DeveloperRegistry.new(
     publicStorage.address,
-    developerRegistryDelegate.address
+    developerRegistryDelegate.address, 
+    botCoinAddress.address
   )
   developerRegistry = _.extend(
     developerRegistry,
@@ -19,3 +23,4 @@ export default async function newDeveloperRegistry () {
   )
   return developerRegistry
 }
+
