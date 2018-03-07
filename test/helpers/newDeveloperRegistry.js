b7732210485b2f94c572c113ea84b26aaf5afaa1
alexfigtree <alexandra.higuera@gmail.com>
2018-03-07 17:51:02 -0500

    Fewer failing tests

--- test/helpers/newDeveloperRegistry.js
+++ test/helpers/newDeveloperRegistry.js
@@ -1,10 +1,12 @@
 /* globals artifacts */
 
 import _ from 'lodash'
+import newBotCoinPaymentRegistry from './newBotCoinPaymentRegistry'
 
 const PublicStorage = artifacts.require('./PublicStorage.sol')
 const DeveloperRegistry = artifacts.require('./DeveloperRegistry.sol')
 const DeveloperRegistryDelegate = artifacts.require('./DeveloperRegistryDelegate.sol')
+const botCoinPaymentReg = artifacts.require('./BotCoinPaymentRegistry.sol')
 
 export default async function newDeveloperRegistry (botCoinAddress, tallaWalletAddress, entryPrice) {
   const publicStorage = await PublicStorage.new()
@@ -14,15 +16,15 @@ export default async function newDeveloperRegistry (botCoinAddress, tallaWalletA
   let developerRegistry = await DeveloperRegistry.new(
     publicStorage.address,
     developerRegistryDelegate.address, 
-    botCoinAddress
   )
   developerRegistry = _.extend(
     developerRegistry,
     await DeveloperRegistryDelegate.at(developerRegistry.address)
   )
 
-  await developerRegistry.setTallaWallet(tallaWalletAddress)
-  await developerRegistry.setEntryPrice(entryPrice)
+
+  await botCoinPaymentReg.setTallaWallet(tallaWalletAddress)
+  await botCoinPaymentReg.setEntryPrice(entryPrice)
 
   return developerRegistry
 }
