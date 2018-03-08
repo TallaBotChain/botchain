84897639c82dab3d18345aa34c55889824bc3831
alexfigtree <alexandra.higuera@gmail.com>
2018-03-08 18:22:02 -0500

    Comments for OwnerRegistry.sol

--- contracts/Registry/OwnerRegistry.sol
+++ contracts/Registry/OwnerRegistry.sol
@@ -7,5 +7,10 @@ import "./Registry.sol";
  * Interface for a registry with entries that own entries in another registry
  */
 contract OwnerRegistry is Registry {
-  function mintingAllowed(address _minter, uint256 _entryId) public view returns (bool _mintingAllowed);
+	/**
+	 * @dev Returns true if minting is allowed
+	 * @param _minter Address of minter
+	 * @param _entryId An id associated with the entry
+	 */
+  	function mintingAllowed(address _minter, uint256 _entryId) public view returns (bool _mintingAllowed);
 }
