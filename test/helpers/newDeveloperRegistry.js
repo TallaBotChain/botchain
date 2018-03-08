d10dcc1f0e2fe6b1d686cf479ec83d93e77eb31d
alexfigtree <alexandra.higuera@gmail.com>
2018-03-08 11:44:19 -0500

    Remove requires

--- test/helpers/newDeveloperRegistry.js
+++ test/helpers/newDeveloperRegistry.js
@@ -1,12 +1,10 @@
 /* globals artifacts */
 
 import _ from 'lodash'
-import newBotCoinPaymentRegistry from './newBotCoinPaymentRegistry'
 
 const PublicStorage = artifacts.require('./PublicStorage.sol')
 const DeveloperRegistry = artifacts.require('./DeveloperRegistry.sol')
 const DeveloperRegistryDelegate = artifacts.require('./DeveloperRegistryDelegate.sol')
-const botCoinPaymentReg = artifacts.require('./BotCoinPaymentRegistry.sol')
 
 export default async function newDeveloperRegistry (botCoinAddress, tallaWalletAddress, entryPrice) {
   const publicStorage = await PublicStorage.new()
