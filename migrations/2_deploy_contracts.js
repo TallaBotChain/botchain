12595a1ada0151a6df0c66ee4e9e8fbcc8e15e44
alexfigtree <alexandra.higuera@gmail.com>
2018-03-13 12:24:05 -0400

    Initial pseudocode

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,26 @@ const BotCoin = artifacts.require('./BotCoin.sol')
 const TokenSubscription = artifacts.require('./TokenSubscription.sol') */
 
 module.exports = function (deployer) {
+
+  /*
+  The script needs to first deploy the following contracts:
+
+    * An instance of PublicStorage
+    * An instance of BotCoin
+    * The four contracts in Delegate dir:
+      - DeveloperRegistryDelegate
+      - BotProductRegistryDelegate
+      - BotServiceRegistryDelegate
+      - BotInstanceRegistryDelegate
+    * Four registry instance contracts. Each instantiated with PublicStorage, delegate, and BotCoin addresses
+      - A DeveloperRegistry instance, with proxy implementation set to DeveloperRegistryDelegate address
+      - A BotEntryRegistry instance, with proxy implementation set to BotProductRegistryDelegate address
+      - A BotEntryRegistry instance, with proxy implementation set to BotServiceRegistryDelegate address
+      - A BotEntryRegistry instance, with proxy implementation set to BotInstanceRegistryDelegate address
+
+  After deployment, script needs to set talla wallet and entry price for each registry. This can be done by executing transactions to each registry instance contract.
+  */
+
   /* deployer.deploy(DeveloperRegistry).then(() => {
     return deployer.deploy(BotCoin)
   }).then(() => {
