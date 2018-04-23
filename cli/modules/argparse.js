375b0d931c06eaa2047b210de798ea0c196e47bc
Henry Wagner <henry@talla.com>
2018-04-23 15:27:11 -0400

    - Moved towards subcommands in argparse - Added functions for approval

--- cli/modules/argparse.js
+++ cli/modules/argparse.js
@@ -6,30 +6,59 @@ const parser = new ArgumentParser({
   description: 'Botchain Management CLI'
 });
 
-const subparsers = parser.addSubparsers({
+const cmdLvl1 = parser.addSubparsers({
   title: 'subcommands',
   dest: 'subcommand_name'
 })
 
-const deploy = subparsers.addParser('deploy',{
+const deploy = cmdLvl1.addParser('deploy',{
   desciption:'All actions related to deploying a BotChain contract to Ethereum.',
   addHelp: true
 });
 
-const get = subparsers.addParser('get',{
+const get = cmdLvl1.addParser('get',{
   desciption:'All actions related to reading the current state of the contracts.',
   addHelp: true
 });
 
-const update = subparsers.addParser('update',{
+const update = cmdLvl1.addParser('update',{
   desciption:'All actions related to updating state in the currently deployed contracts.',
   addHelp: true
 });
 
-const registration = subparsers.addParser('registration',{
+const registration = cmdLvl1.addParser('registration',{
   desciption:'All actions related to registering an entity on BotChain.',
   addHelp: true
 });
+
+const regCmds = registration.addSubparsers({
+  title: 'Registration Commands',
+  parents: [registration],
+  dest: 'registration'
+})
+
+const regApprove = regCmds.addParser('approve',{
+  desciption:'Approve a developer that has previously registered.',
+  addHelp: true
+});
+
+regApprove.addArgument(
+  [ '-a', '--address' ],
+  {
+    help: 'Approve a developer at the provided address.',
+    defaultValue: '0x0',
+    nargs: 1
+  }
+);
+
+regApprove.addArgument(
+  [ '-p', '--password' ],
+  {
+    help: 'Password for the Network Management Address.',
+    nargs: 1,
+    required: true
+  }
+);
 
 parser.addArgument(
   [ '-i', '--rpc' ],
@@ -67,29 +96,11 @@ update.addArgument(
   }
 );
 
-registration.addArgument(
-  [ '-p', '--password' ],
-  {
-    help: 'Password for the Network Management Address.',
-    nargs: 1,
-    required: true
-  }
-);
-
 deploy.addArgument(
   [ '-i', '--impl' ],
   {
     help: 'Deploy one of the supported contracts to currently connected network.',
     choices: ['dev','bot','instance','service'],
-    nargs: 1
-  }
-);
-
-registration.addArgument(
-  [ '-a', '--approve' ],
-  {
-    help: 'Approve a developer at the provided address.',
-    defaultValue: '0x0',
     nargs: 1
   }
 );
