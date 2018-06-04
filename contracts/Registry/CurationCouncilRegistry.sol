829d670409c10d699e9b812b0be9ecab3f1545ca
Jay Byoun <jay@jaybyoun.com>
2018-06-03 23:39:51 -0700

    refactor createRgistrationVote

--- contracts/Registry/CurationCouncilRegistry.sol
+++ contracts/Registry/CurationCouncilRegistry.sol
@@ -50,12 +50,12 @@ contract CurationCouncilRegistry is OwnableRegistry {
     return _storage.getUint(keccak256("registrationVoteNayCount", registrationVoteId))
   }
 
-  function incrementYayCount(uint256 registrationVoteId) private  {
+  function increaseYayCount(uint256 registrationVoteId) private  {
     uint256 currentYayCount = getYayCount(registrationVoteId)
     _storage.setUint(keccak256("registrationVoteYayCount", registrationVoteId), currentYayCount + 1)
   }
 
-  function incrementNayCount(uint256 registrationVoteId) private  {
+  function increaseNayCount(uint256 registrationVoteId) private  {
     uint256 currentNayCount = getNayCount(registrationVoteId)
     _storage.setUint(keccak256("registrationVoteNayCount", registrationVoteId), currentNayCount + 1)
   }
@@ -84,32 +84,14 @@ contract CurationCouncilRegistry is OwnableRegistry {
     public 
   {
     require(developerAddress != 0x0);
-    /** Need clarification on how to handle developers who have been denied */
-    require(!registrationVoteExists(developerAddress));
+    // Need clarification on how to handle developers who have been denied 
+    // require(!registrationVoteExists(developerAddress));
 
-    uint256 registrationVoteId = totalSupply().add(1);
-
-    setInitialRegistrationVote(registrationVoteId, developerAddress);
-  }
-
-  function castRegistrationVote(uint256 registrationVoteId, bool vote) public {
-    uint256 currentYayCount = 
-    if vote {
-      incrementYayCount(registrationVoteId)
-    } else {
-      incrementNayCount(registrationVoteId)
-    }
-  }
-
-  /**
-  * @dev Sets initial registration vote data
-  * @param registrationVoteId An id associated with the registration vote
-  * @param developerAddress address of developer requesting registration approval
-  */
-  function setInitialRegistrationVote(uint256 registrationVoteId, address developerAddress) private {
     uint256 initialBlock = block.number
     uint256 finalBlock = initialBlock + 100000
+    uint256 registrationVoteId = totalSupply().add(1);
 
+    _mint(developerAddress, registrationVoteId);
     _storage.setAddress(keccak256("registrationVoteDeveloperAddress", registrationVoteId), developerAddress);
     _storage.setUint(keccak256("registrationVoteiInitialBlock", registrationVoteId), initialBlock);
     _storage.setUint(keccak256("registrationVoteiFinalBlock", registrationVoteId), finalBlock);
@@ -117,6 +99,16 @@ contract CurationCouncilRegistry is OwnableRegistry {
     _storage.setUint(keccak256("registrationVoteNayCount", registrationVoteId), 0);
 
     RegistrationVoteCreated(registrationVoteId, initialBlock, finalBlock, 0, 0, false);
+  }
+
+  function castRegistrationVote(uint256 registrationVoteId, bool vote) public {
+    uint256 currentYayCount = getYayCount(registrationVoteId)
+    uint256 currentNayCount = getNayCount(registrationVoteId)
+    if vote {
+      incrementYayCount(registrationVoteId, getStakeAmount(msg.sender))
+    } else {
+      incrementNayCount(registrationVoteId, getStakeAmount(msg.sender))
+    }
   }
 
 
