92c780d26862b46fa4fe7e5af934b89a6cb53b18
Henry Wagner <henry@talla.com>
2018-06-22 14:14:21 -0400

    update tests to use async calls

--- contracts/Delegates/TokenVaultDelegate.sol
+++ contracts/Delegates/TokenVaultDelegate.sol
@@ -5,6 +5,12 @@ import '../Upgradability/BaseStorage.sol';
 import '../Upgradability/StorageConsumer.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
+/**
+* @title TokenVaultDelegate
+* @dev Delegate contract which implements functions to store and manipulate the balances related to
+*  curator and developer incentives. It maps the balances using the IncentiveMap inherited from
+*  the TokenVault interface.
+*/
 contract TokenVaultDelegate is TokenVault {
 
   function TokenVaultDelegate(BaseStorage _storage, address _arbiter)
@@ -13,7 +19,8 @@ contract TokenVaultDelegate is TokenVault {
     {}
 
   /**
-  *  @dev Retreives the address of the EIP-20 from storage and instanties a StandardToken interface for it.
+  *  @dev Retreives the address of the EIP-20 from storage and instanties a StandardToken interface 
+  *   for it.
   *  @return StandardToken interface to the EIP-20 token contract.
   */
   function botcoin() public returns (StandardToken) {
@@ -21,60 +28,129 @@ contract TokenVaultDelegate is TokenVault {
   }
 
   /**
-  * @dev Applies a reward to the balance of a curator address.
+  * @dev Reserves tokens from the vaults balance for a particular curator.  The reserved tokens 
+  *  are reflected in the current balance of the curator.  The TokenVaults available balance and
+  *  reserved balance will also reflect the change, but the vault's overall balance will remain
+  *  unchanged until the curator collects the reward.
+  *  Can only be executed by the Arbiter of the Vault (ideally the CurationCouncil contract).
   */
   function applyCuratorReward() onlyArbiter public {
-    // TODO: this should be done by fetching a value stored by the governance board
-    //       It can't currently be done this way because storage getters don't support
-    //       alternative scoping for fetches.
     reserveTokens(curatorRewardRate());
-    setBalance(tx.origin, this.balance() + curatorRewardRate());
-    BalanceUpdate(this.balance());
+    setBalance(tx.origin, balance() + curatorRewardRate());
+    BalanceUpdate(balance());
   }
 
+  /**
+  * @dev Sends token reward from the vaults balance to the address of the transaction intiator.
+  *  The TokenVaults available balance also reflect the change.
+  *  Can only be executed by the Arbiter of the Vault (ideally the CurationCouncil contract).
+  */
   function applyDeveloperReward() onlyArbiter public {
-    require(devRewardRate() <= availableBalance());
-    botcoin().transfer(tx.origin, devRewardRate());
+    require(developerRewardRate() <= availableBalance());
+    require(botcoin().transfer(tx.origin, developerRewardRate()));
   }
 
+  /**
+  * @dev Returns the balance reflected in the Botcoin contract minus the balances
+  *  that have already been reserved for curator rewards.
+  */
   function availableBalance() private returns (uint) {
     return vaultBalance() - reservedBalance();
   }
 
+  /**
+   * @dev Returns the current amount of the Vault's Botcoin balance that has been reserved
+   *  for curator rewards.
+   */
   function reservedBalance() public returns (uint) {
     return _storage.getUint('reservedBalance');
   }
 
+  /**
+   * @dev Reserves the provided amount of tokens to be collected at future time.
+   *  The available balance must be large enough to cover the requested amount.
+   * @param amount the number of tokens to reserve for the transaction origin.
+   */
   function reserveTokens(uint amount) private {
     uint new_reserve = amount + reservedBalance();
-    require(new_reserve <= vaultBalance());
-    _storage.setUint('reserveBalance', new_reserve);
+    require(new_reserve >= amount && new_reserve <= vaultBalance());
+    _storage.setUint('reservedBalance', new_reserve);
   }
 
+  /**
+  * @dev Updates the reserved balance to reflect the change.
+  *  The amount to consume must be less than or equal to the current reserved
+  *  balance.
+  * @param amount the count of tokens in the reserve balance to consume.
+  */
+  function consumeReservedTokens(uint amount) private {
+    uint reserved = reservedBalance();
+    require(amount <= reserved);
+    _storage.setUint('reservedBalnace', reserved - amount);
+  }
+
+  /**
+   * @dev Returns the current reward rate that can be claimed by a curator for doing
+   *  work.
+   */
   function curatorRewardRate() public returns (uint) {
+    // TODO: This should be returning a value based on an asymptotic function
+    // rather than an absolute value. What's the appropriate function?
+    // I.E. return calculateReward(curatorRewardRate())
     return _storage.getUint('curatorEmissionRate');
   }
 
+  /**
+   * @dev Sets the reward rate that can be claimed by a curator for doing work.
+   *  This can only be performed by the Owner of the Vault (ideally the GovernanceBoard).
+   * @param rate the count of tokens that should be rewarded.
+   */
   function setCuratorRewardRate(uint rate) onlyOwner public {
     _storage.setUint('curatorEmissionRate', rate);
   }
 
-  function setDevRewardRate(uint rate) onlyOwner public {
-    _storage.setUint('curatorEmissionRate', rate);
+  /**
+   * @dev Sets the reward rate that can be claimed by a developer for successfully registering.
+   *  This can only be performed by the Owner of the Vault (ideally the GovernanceBoard).
+   * @param rate the count of tokens that should be rewarded.
+   */
+  function setDeveloperRewardRate(uint rate) onlyOwner public {
+    _storage.setUint('developerEmissionRate', rate);
   }
 
-  function devRewardRate() private returns (uint) {
-    return _storage.getUint('devEmissionRate');
+  /**
+   * @dev Returns the current developer reward rate available for registrations.
+   */
+  function developerRewardRate() public returns (uint) {
+    return _storage.getUint('developerEmissionRate');
   }
 
+  /**
+   * @dev Returns the current number of Botcoin stored in the TokenVault. This value does not
+   * reflect the number of tokens that are reserved for curator rewards.
+   */
   function vaultBalance() internal returns (uint) {
     return botcoin().balanceOf(address(this));
   }
 
-  function collectCuratorReward() internal {
-    uint _balance = this.balance();
-    require(_balance <= vaultBalance());
+  /**
+   * @dev Resets the balance of the transaction origin to zero and consumes them from the reserves.
+   * This should only be called after the balance has been succesfully transfered.
+   */
+  function resetCuratorBalance() private {
+    consumeReservedTokens(balance());
+    setBalance(tx.origin, 0);
+    BalanceUpdate(balance());
+  }
+
+  /**
+   * @dev Transfers the balance in the TokenVault to the wallet of the transactions origin.
+   *  Use caution not to call this on empty balances, since this will still require gas.
+   */
+  function collectCuratorReward() public {
+    uint _balance = balance();
+    require(_balance <= reservedBalance());
     require(botcoin().transfer(tx.origin, _balance));
-    BalanceUpdate(this.balance());
+    resetCuratorBalance();
   }
 }
--- test/TokenVault.test.js
+++ test/TokenVault.test.js
@@ -16,8 +16,10 @@ const zeroAddr = '0x0000000000000000000000000000000000000000'
 contract('TokenVault', () => {
   let tokenVault, tokenVaultDelegate, botcoin, publicStorage, curationCouncil
   let accounts, arbiter, owner, curator, voter
+  let vault_balance, reward_rate
 
   before(async () => {
+
     // We're relying on deterministic addr generation, if you add
     // any new contracts add them to the end of the list.
     accounts = await web3.eth.getAccounts()
@@ -25,6 +27,7 @@ contract('TokenVault', () => {
     arbiter = accounts[1]
     curator = accounts[2]
     voter = accounts[3]
+    reward_rate = 1
     publicStorage = await PublicStorage.new({from: owner})
     botcoin = await BotCoin.new({from:owner})
     curationCouncil = await CurationCouncil.new(publicStorage.address, botcoin.address, {from:owner})
@@ -49,12 +52,13 @@ contract('TokenVault', () => {
       expect(curationCouncil.address).to.equal('0x0237443359ab0b11ecdc41a7af1c90226a88c70f')
     })
     it('TokenVault should be 0x0000000000000000000000000000000000000000', async () => {
-      curationCouncil.tokenVault.call({from: owner}).then((ctv) => {
-        expect(ctv).to.equal(zeroAddr)
-      })
+      return curationCouncil.tokenVault.call({from: owner})
+        .then((ctv) => {
+          expect(ctv).to.equal(zeroAddr)
+        })
     })
     it('can add token vault', async () => {
-      curationCouncil.changeTokenVault(tokenVault.address, {from: owner})
+      return curationCouncil.changeTokenVault(tokenVault.address, {from: owner})
         .then(() => {
           return curationCouncil.tokenVault.call({from: owner})
         }).then((ctv) => {
@@ -76,39 +80,83 @@ contract('TokenVault', () => {
     })
 
     it('owner can set curator reward rate', async () => {
-      let new_rate = 1
-      tokenVault.setCuratorRewardRate(new_rate, {from:owner})
+      return tokenVault.setCuratorRewardRate(reward_rate, {from:owner})
         .then(() => {
           return tokenVault.curatorRewardRate.call()
         })
         .then((rate) => {
-          expect(rate.toNumber()).to.equal(new_rate)
+          expect(rate.toNumber()).to.equal(reward_rate)
+        })
+    })
+
+    it('owner can set developer reward rate', async () => {
+      return tokenVault.setDeveloperRewardRate(reward_rate, {from:owner})
+        .then(() => {
+          return tokenVault.developerRewardRate.call()
+        })
+        .then((rate) => {
+          expect(rate.toNumber()).to.equal(reward_rate)
         })
     })
 
     it('received 10,000 BOTC', async () => {
-      let token_count = 10;
-      botcoin.transfer(tokenVault.address, token_count, {from: owner})
-      .then(() => {
-        return botcoin.balanceOf(tokenVault.address)
-      })
-      .then((balance) => {
-        expect(balance.toNumber()).to.equal(token_count)
-      })
+      let token_count = 10000;
+      return botcoin.transfer(tokenVault.address, token_count, {from: owner})
+        .then(() => {
+          return botcoin.balanceOf(tokenVault.address)
+        })
+        .then((balance) => {
+          vault_balance = balance.toNumber()
+          expect(balance.toNumber()).to.equal(token_count)
+        })
     })
 
     it('curationCouncil can apply reward for curator', async () => {
       let initial_balance
-      tokenVault.balance.call({from:voter})
+      return tokenVault.balance.call({from:voter})
         .then((_initial_balance) => {
           initial_balance = _initial_balance;
           return curationCouncil.vote({from: voter})
         })
         .then(() => {
-          return tokenVault.balance({from: voter})
+          console.log('bullshit')
+          return tokenVault.balance.call({from: voter})
         })
         .then((final_balance) => {
+          console.log('bullshit2:',final_balance)
           expect(initial_balance.toNumber()).to.be.lessThan(final_balance.toNumber())
+        })
+    })
+
+    it('voter can collect rewards', async () => {
+      let wallet_balance
+      return tokenVault.balance.call({from:voter})
+        .then((_vault_balance) => {
+          expect(_vault_balance.toNumber()).to.equal(reward_rate)
+          return botcoin.balanceOf.call(voter)
+        })
+        .then((_wallet_balance) => {
+          wallet_balance = _wallet_balance
+          return tokenVault.collectCuratorReward({from:voter})
+        })
+        .then(() => {
+          return tokenVault.balance.call({from:voter})
+        })
+        .then((_user_vault_balance) => {
+          // Rewards have been collected to wallet, the users vault
+          // balance should be empty.
+          expect(_user_vault_balance.toNumber()).to.equal(0)
+          return botcoin.balanceOf.call(voter)
+        })
+        .then((_wallet_balance) => {
+          expect(_wallet_balance.toNumber()).to.be.greaterThan(wallet_balance.toNumber())
+        })
+    })
+
+    it('vault balance updated correctly', async () => {
+      return botcoin.balanceOf(tokenVault.address)
+        .then((balance) => {
+          expect(balance.toNumber()).to.equal(vault_balance - reward_rate)
         })
     })
   })
