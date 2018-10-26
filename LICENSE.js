bceddf7b8b43c4022e16b5443a7c1ac5ff21d3ed
hcwagner <35582501+hcwagner@users.noreply.github.com>
2018-10-26 10:00:30 -0400

    Delete LICENSE.js
    We are not releasing under MIT

--- LICENSE.js
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * https://github.com/TallaBotChain/botchain/
- * Copyright (c) 2017-2018 Talla, Inc.
- * Distributed under the MIT software license, see the accompanying
- * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
- */
